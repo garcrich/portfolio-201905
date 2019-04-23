e3f3d1319c9c5535b21cdab3b2e1cc0a804dba41
Ricky Garcia <rg3646@outlook.com>
2019-04-22 23:59:46 -0600

    Add Google font link

--- components/Template/PageWrapper.js
+++ components/Template/PageWrapper.js
@@ -1,11 +1,15 @@
 import Nav from './Nav';
 import Footer from './Footer';
 import '../../styles/styles.scss';
+import Head from 'next/head';
 
 class PageWrapper extends React.Component {
     render() {
     return (
     <React.Fragment>
+        <Head>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Playfair+Display:400,900" rel="stylesheet"/>
+        </Head>
         <Nav/>
         {this.props.children}
         <Footer/>
